db80ee1f17cd4cb51f1b7d30d74ec734741c6130
rudra <rudrakhandelwal1212@gmail.com>
2024-02-17 22:59:32 +0530

    get and post request updated

--- index.js
+++ index.js
@@ -446,13 +446,55 @@ app.post("/applyforinternship", async (req, res) => {
     });
   
   // login with password
-    app.post(
-      "/login2",
-      passport.authenticate("local", {
-        successRedirect: "/explore2",
-        failureRedirect: "/login2",
-      })
-    );
+    // app.post(
+    //   "/login2",
+    //   passport.authenticate("local", {
+    //     successRedirect: "/explore2",
+    //     failureRedirect: "/login2",
+    //   })
+    // );
+
+    app.post("/login2",
+    
+    async(req, res) => {
+      const email = req.body.email;
+      const loginPassword = req.body.password;
+    
+      try {
+        const checkResult = await db.query("SELECT password FROM recruiter WHERE email = $1", 
+              [email] );
+        
+        if (checkResult.rows.length > 0) {
+          const hashedPassword = checkResult.rows[0].password;
+          console.log("hashedPassword: " + hashedPassword);
+          bcrypt.compare(loginPassword, hashedPassword, (err, result) => {
+            if (err) {
+              console.error('Error comparing passwords:', err);
+              // Handle error
+              res.sendStatus(500);
+            } else if (result) {
+              // Passwords match, user authentication successful
+              console.log('Password matches!');
+              // Proceed with login
+              res.sendStatus(200);
+            } else {
+              // Passwords do not match, user authentication failed
+              console.log('Password does not match!');
+              // Handle incorrect password
+              res.sendStatus(401);
+            }
+          });
+
+        } else {
+          res.sendStatus(401);
+        }
+        
+      }catch (err) {
+        console.log(err);
+      }
+    }
+
+  );
   
    // company details page 
 
@@ -462,37 +504,42 @@ app.post("/applyforinternship", async (req, res) => {
 
 // company registration 
 app.post("/register2", async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-  
-    try {
-      const checkResult = await db.query("SELECT * FROM recruiter WHERE email = $1", [
-        email,
-      ]);
-  
-      if (checkResult.rows.length > 0) {
-        req.redirect("/login2");
-      } else {
-        bcrypt.hash(password, saltRounds, async (err, hash) => {
-          if (err) {
-            console.error("Error hashing password:", err);
-          } else {
-            const result = await db.query(
-              "INSERT INTO recruiter (email, password) VALUES ($1, $2) RETURNING *",
-              [email, hash]
-            );
-            const user = result.rows[0];
-            req.login(user, (err) => {
-              console.log("success");
-              res.redirect("/companydetails");
-            });
-          }
-        });
-      }
-    } catch (err) {
-      console.log(err);
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
+    const checkResult = await db.query("SELECT * FROM recruiter WHERE email = $1", [
+      email,
+    ]);
+
+    if (checkResult.rows.length > 0) {
+      // Company with the given email already exists, handle accordingly
+      res.sendStatus(400);
+    } else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error("Error hashing password:", err);
+
+          res.sendStatus(500);
+
+        } else {
+          const result = await db.query(
+            "INSERT INTO recruiter (email, password) VALUES ($1, $2) RETURNING *",
+            [email, hash]
+          );
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log("success");
+            res.sendStatus(201);
+            
+          });
+        }
+      });
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 
 // manual registration verification using password
 passport.use(
@@ -594,27 +641,31 @@ passport.use(
 
 app.post("/companydetails", async (req, res) => {
   const reqPayload = req.body;
- console.log('Data received: ', reqPayload);
- const company_name = reqPayload.companyname;
- const qualification_required  = reqPayload.qualification;
- const contact_no  = reqPayload.contactnumber;
- const  position_name  = reqPayload.position;
- const skills_required  = reqPayload.skills;
- const job_description  = reqPayload.jd;
- const email  = reqPayload.email;
- const locations = reqPayload.location;
- const interested_domain  = reqPayload.interesteddomain;
+  console.log('Data received: ', reqPayload);
+
+  const company_name = reqPayload.companyname;
+  const qualification_required  = reqPayload.qualification;
+  const contact_no  = reqPayload.contactnumber;
+  const position_name  = reqPayload.position;
+  const skills_required  = reqPayload.skills;
+  const job_description  = reqPayload.jd;
+  const email  = reqPayload.email;
+  const locations = reqPayload.location;
+  const interested_domain  = reqPayload.interesteddomain;
+
+  
+    const result = await db.query(
+      "INSERT INTO company_details (company_name, qualification_required, contact_no, position_name, skills_required, job_description, email, locations, interested_domain) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);",
+      [company_name, qualification_required, contact_no, position_name, skills_required, job_description, email, locations, interested_domain]
+    );
+    console.log(result);
+    console.log(email);
+
+    res.sendStatus(200);
+
+});
 
 
-   const result = await db.query(
-     "INSERT INTO company_details (company_name , qualification_required, contact_no, position_name, skills_required, job_description, email, locations, interested_domain) VALUES( $1, $2, $3, $4,$5, $6, $7, $8, $9);",
-     [company_name , qualification_required, contact_no, position_name, skills_required, job_description, email, locations, interested_domain]
-   );
- console.log(result);
- console.log(email);
-
- res.sendStatus(200);
- });
 
    // const id = result.rows[0].id;
    // currentUserId = id;
@@ -631,69 +682,102 @@ app.post("/companydetails", async (req, res) => {
 
     // COMPANY POST REQUEST TO POST INTERNSHIP
 
-app.post("/postinternship", async (req, res) => {
+    app.post("/postinternship", async (req, res) => {
+      try {
+        const reqPayload = req.body;
+        console.log('Internship Posted Data received: ', reqPayload);
+    
+        const company_name = reqPayload.companyname;
+        const qualification_required = reqPayload.qualification;
+        const contact_no = reqPayload.contactnumber;
+        const position_name = reqPayload.position;
+        const skills_required = reqPayload.skills;
+        const job_description = reqPayload.jd;
+        const email = reqPayload.email;
+        const locations = reqPayload.location;
+        const interested_domain = reqPayload.interesteddomain;
+    
+        // Insert the internship post details into the database
+        const result = await db.query(
+          "INSERT INTO internship_post (company_name, qualification_required, contact_no, position_name, skills_required, job_description, email, locations, interested_domain) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);",
+          [company_name, qualification_required, contact_no, position_name, skills_required, job_description, email, locations, interested_domain]
+        );
+    
+        console.log(result);
+        console.log(email);
+    
+        // Notify students about the new internship opportunity (you can implement this as needed)
+    
+        res.status(200).json({ success: "Internship posted successfully!" });
+      } catch (error) {
+        console.error("Error posting internship:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.get("/companydetails/:email", async (req, res) => {
+
+      try {
+        const email = req.params.email;
+        console.log("Email: " + email);
+    
+        // Fetch company details from the database based on the student email
+        const result = await db.query(
+          'SELECT company_name, qualification_required, contact_no, position_name, skills_required, job_description, email, locations, interested_domain FROM company_details WHERE email = $1',
+          [email]
+        );
+    
+        // Check if a company with the given email exists
+        if (result.rows.length === 0) {
+          return res.status(404).json({ error: "Company not found" });
+        }
+    
+        // Extract company details from the query result
+        const companyDetails = result.rows[0];
+    
+        // Send the company details as a response
+        res.status(200).json(companyDetails);
+        console.log('Company Details:', companyDetails );
+      } catch (error) {
+        console.error("Error fetching company details:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+
+    // get company posted internship
+app.get("/getinternshipinfo", async (req, res) => {
   try {
-    const reqPayload = req.body;
-    console.log('Internship Post Data received: ', reqPayload);
+      // Retrieve specific internship post information from the database
+      const internshipInfo = await db.query(
+          "SELECT * FROM internship_post;"
+      );
 
-    const company_name = reqPayload.companyname;
-    const qualification_required = reqPayload.qualification;
-    const contact_no = reqPayload.contactnumber;
-    const position_name = reqPayload.position;
-    const skills_required = reqPayload.skills;
-    const job_description = reqPayload.jd;
-    const email = reqPayload.email;
-    const locations = reqPayload.location;
-    const interested_domain = reqPayload.interestedinternship;
+      console.log("Internship Information:", internshipInfo.rows);
 
-    // Insert the internship post details into the database
-    const result = await db.query(
-      "INSERT INTO internship_post (company_name, qualification_required, contact_no, position_name, skills_required, job_description, email, locations, interested_domain) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);",
-      [company_name, qualification_required, contact_no, position_name, skills_required, job_description, email, locations, interested_domain]
-    );
-
-    console.log(result);
-    console.log(email);
-
-    // Notify students about the new internship opportunity (you can implement this as needed)
-
-    res.status(200).json({ success: "Internship posted successfully!" });
+      res.status(200).json(internshipInfo.rows);
   } catch (error) {
-    console.error("Error posting internship:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+      console.error("Error getting internship information:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
-app.get("/companydetails/:email", async (req, res) => {
+// get posted students new internship
 
+app.get("/getappliedinternshipinfo", async (req, res) => {
   try {
-    const email = req.params.email;
-    console.log("Email: " + email);
+      // Retrieve specific internship post information from the database
+      const sudentInfo = await db.query(
+          "SELECT * FROM internship_application;"
+      );
 
-    // Fetch company details from the database based on the company_id
-    const result = await db.query(
-      "SELECT company_name, qualification_required, contact_no, position_name, skills_required, job_description, email, locations, interested_domain FROM company WHERE email = $1",
-      [email]
-    );
+      console.log("Applied Students Information:", sudentInfo.rows);
 
-    // Check if a company with the given company_id exists
-    if (result.rows.length === 0) {
-      return res.status(404).json({ error: "Company not found" });
-    }
-
-    // Extract company details from the query result
-    const companyDetails = result.rows[0];
-
-    // Send the company details as a response
-    res.status(200).json(companyDetails);
-    console.log('Company Details:', companyDetails );
+      res.status(200).json(sudentInfo.rows);
   } catch (error) {
-    console.error("Error fetching company details:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+      console.error("Error getting applied students information:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+  }
 });
-
-
-
 
     
